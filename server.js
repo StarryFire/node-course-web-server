c4ca5d745270347512ed98c5f7eadf7013cbd7ee
Kartik Sharma <alpha.resonate@gmail.com>
2018-05-02 23:07:00 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 //app.use(express.static(__dirname + '/public'));
 
@@ -21,10 +22,12 @@ app.use( (req, res, next) => {
     next();
 });
 
+/*
 app.use( (req, res, next) => {
     res.render('maintenance.hbs');
 
-})
+});
+*/
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -47,6 +50,6 @@ app.get('/about', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
